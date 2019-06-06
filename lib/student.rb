eed1dbde4ecc584fec6b5e1c44f6ed32c745d62e
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 14:58:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,12 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def initialize
+  end
+
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+
   end
 
   def self.all
@@ -13,17 +17,21 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = name
+    SQL
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
