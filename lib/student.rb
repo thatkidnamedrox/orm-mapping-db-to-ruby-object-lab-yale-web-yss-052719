39c376bb7991304b8ddd152cdbaf0d6ef59049a4
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-06 15:16:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,6 +44,14 @@ class Student
       SELECT * FROM students
       WHERE grade < 12
     SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
+    SQL
     self.new_from_db(DB[:conn].execute(sql))
   end
 
